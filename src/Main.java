d844ae62bcddf6d05a618b9cf224f076eb223f30
Damien <1204electro@gmail.com>
2025-01-08 17:04:20 -0500

    Added javadoc

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ public class Main {
         System.out.printf("The %d term of the Fibonacci Sequence is: %d", 10, sequence(10));
     }
 
+    /**
+     * This method calculates the fibonacci sequence to the nth term then returns the value found.
+     * @param n the number of times the sequence will iterate
+     * @return the value found at the nth term.
+     */
     public static int sequence(int n) {
         if (n <= 1)
             return n;
